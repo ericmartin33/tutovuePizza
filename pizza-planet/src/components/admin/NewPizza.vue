23bb35da99ba868549b9f97935c82541f9c7ec24
ericmartin33 <7yx9zUneaHPjn3g>
2023-12-09 18:52:27 +0100

    new Pizza data binding

--- pizza-planet/src/components/admin/NewPizza.vue
+++ pizza-planet/src/components/admin/NewPizza.vue
@@ -6,34 +6,34 @@
         <form>
             <div class="form_group">
                 <label for="name">Name</label>
-                <input type="text" id="name" />
+                <input type="text" id="name" v-model="newPizza.name" />
             </div>
 
             <div class="form_group">
                 <label for="description">Description</label>
-                <textarea rows="5" id="description"></textarea>
+                <textarea rows="5" id="description" v-model="newPizza.description"></textarea>
             </div>
             <p>
                 <strong>Option 1</strong>
             </p>
             <div class="form_group">
                 <label for="size">Size (cm)</label>
-                <input type="text" id="size1" />
+                <input type="text" id="size1" v-model="newPizza.options[0].size" />
             </div>
             <div class="form_group">
                 <label for="price1">Price</label>
-                <input type="text" id="price1" />
+                <input type="text" id="price1" v-model="newPizza.options[0].price" />
             </div>
             <p>
                 <strong>Option 2</strong>
             </p>
             <div class="form_group">
                 <label for="size">Size (cm)</label>
-                <input type="text" id="size1" />
+                <input type="text" id="size1" v-model="newPizza.options[1].size" />
             </div>
             <div class="form_group">
                 <label for="price1">Price</label>
-                <input type="text" id="price1" />
+                <input type="text" id="price1" v-model="newPizza.options[1].price" />
             </div>
             <div class="form_group">
                 <button>Add</button>
@@ -41,3 +41,14 @@
         </form>
     </section>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const newPizza = ref({
+    name: 'eg. Margherita',
+    description: 'Eg. delicious tomato based pizza',
+    options: [{ size: 9, price: 9.99 }, { size: 12, price: 99 }]
+})
+
+</script>
